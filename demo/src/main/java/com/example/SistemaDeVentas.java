bbc5492a0c7194e241ef388320261b72db03c2a9
Juber <juberadrianpezope@gmail.com>
2025-06-14 13:36:35 -0500

    Prueba de commits

--- demo/src/main/java/com/example/SistemaDeVentas.java
+++ demo/src/main/java/com/example/SistemaDeVentas.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class SistemaDeVentas {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Recien creado clase para el proyecto java ");
     }
 }
