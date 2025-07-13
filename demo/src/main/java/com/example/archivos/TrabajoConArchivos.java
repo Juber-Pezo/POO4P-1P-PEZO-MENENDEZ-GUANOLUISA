920815afd3f679951847eacb85f6e8e6cb82cb38
AnaiMenendez <anaimene@espol.edu.ec>
2025-07-13 15:37:18 -0500

    actualizacion de Trabajo con archivos

--- demo/src/main/java/com/example/archivos/TrabajoConArchivos.java
+++ demo/src/main/java/com/example/archivos/TrabajoConArchivos.java
@@ -1,5 +1,7 @@
 package com.example.archivos;
 
+import com.example.Pedido;
+
 /**
  * Clase TrabajoArchivo
  * 
@@ -13,12 +15,21 @@ public class TrabajoConArchivos {
      * @param arg Argumentos de línea de comando (no utilizados)
      */
     public static void main(String[] arg) {
-        ManejoArchivos.EscribirArchivo("archivosalida.txt","Hola!");
+        /* ManejoArchivos.EscribirArchivo("archivosalida.txt","Hola!");
         ManejoArchivos.EscribirArchivo("archivosalida.txt","Adios!");
         ManejoArchivos.EscribirArchivo("archivosalida.txt","NUevo!");
-        ManejoArchivos.LeeFichero("archivosalida.txt");
+        ManejoArchivos.LeeFichero("archivosalida.txt"); */
   
-        
+        // Prueba de los archivos....
+
+        /* ManejoArchivos.LeeFichero("Clientes.txt");
+        ManejoArchivos.LeeFichero("Productos.txt");
+        ManejoArchivos.LeeFichero("Repartidores.txt");
+        ManejoArchivos.LeeFichero("Usuarios.txt"); */
+
+        // Prueba de contador....
+        Pedido p1=new Pedido(null, null, 0, 0, null, null, null);
+        ManejoArchivos.EscribirArchivo("archivosalida.txt",p1.toString());
         
     }
 
