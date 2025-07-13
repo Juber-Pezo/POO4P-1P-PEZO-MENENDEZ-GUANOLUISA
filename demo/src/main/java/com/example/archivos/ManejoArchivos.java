a7406b02ca29661ff54615cf13b433c5330f5946
AnaiMenendez <anaimene@espol.edu.ec>
2025-07-13 15:23:07 -0500

    se añadio un metodo nuevo

--- demo/src/main/java/com/example/archivos/ManejoArchivos.java
+++ demo/src/main/java/com/example/archivos/ManejoArchivos.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.archivos;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -80,5 +80,44 @@ public class ManejoArchivos {
         }
     }
 
+    // para contador
+    public static ArrayList<String> LeeFicheroCont(String nombrearchivo) {
+        ArrayList<String> lineas = new ArrayList<>();
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            archivo = new File(nombrearchivo); // Ajuste -> + demo/
+            fr = new FileReader(archivo,StandardCharsets.UTF_8);
+            br = new BufferedReader(fr);
+
+            // Lectura del fichero
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                //System.out.println(linea); // Comentarlo si no quiero q se muestren los datos
+                lineas.add(linea);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta 
+            // una excepcion.
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return lineas;
+
+    }
 
 }
